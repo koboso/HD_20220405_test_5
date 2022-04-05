6383609cc2791fb51ab8c5cb209d4733df45a638
koboso <34165501+koboso@users.noreply.github.com>
2022-04-05 20:42:50 +0900

    문제해결
    문제해결방법완료.

--- Test_5/Test_5/Program.cs
+++ Test_5/Test_5/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 
-namespace Test_5
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+class Program{
+    static void Main(string[] args){
+/*
+6. 반복문을 이용하여 아래 별을 출력하세요
+        *   *
+         * *
+          *
+         * *
+        *   *
+*/
+
+        for(int i= 0; i< 5; i++){
+            for(int k = 0; k < 5; k++){
+                /*
+                if (i == k)Console.Write(i + k);
+                else if(k+i == 4) Console.Write(i + k);
+                else Console.Write(" ");
+                */
+
+                if (i == k)
+                {
+                    Console.Write("*");
+                }
+                else if((i+k)%2 ==0){
+                    if((i + k) == 6){
+                        Console.Write("*");
+                    }
+
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.Write("\n");
         }
+
+
     }
 }
